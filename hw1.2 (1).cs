21317bc0d0880ddd1a75b6a31fa873b789a0bb95
SonyaKhokhlova <natasha.hohlova73@gmail.com>
2024-10-15 19:24:38 +0300

    03.10.

---
+++ hw1.2 (1).cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a, b;
+            try
+            {
+                string x = Console.ReadLine();
+                a = Convert.ToByte(x);
+                string y = Console.ReadLine();
+                b = Convert.ToByte(y);
+                Console.WriteLine($"{a / b}");
+            }
+            catch
+            {
+                Console.WriteLine("Некорректный ввод!");
+            }
+        }
+    }
+}
